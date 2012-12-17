c256ee8ffb89fd6ccf9136a16e92a300b571ce7a
Nick Bartkowiak <nickbart@gmail.com>
2012-12-17 13:27:08 -0600

    Adding in the photo section stub.

---
+++ Server/Library/Section/Photo.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Plex Server Library Photo Section
+ * 
+ * @category php-plex
+ * @package Plex_Server
+ * @subpackage Plex_Server_Library
+ * @author <nickbart@gmail.com> Nick Bartkowiak
+ * @copyright (c) 2012 Nick Bartkowiak
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public Licence (GPLv3)
+ * @version 0.0.1
+ *
+ * This file is part of php-plex.
+ * 
+ * php-plex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * php-plex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * Class that represents a Plex library photo section and makes available the
+ * retrieval of library photo items.
+ * 
+ * @category php-plex
+ * @package Plex_Server
+ * @subpackage Plex_Server_Library
+ * @author <nickbart@gmail.com> Nick Bartkowiak
+ * @copyright (c) 2012 Nick Bartkowiak
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public Licence (GPLv3)
+ * @version 0.0.1
+ */
+class Plex_Server_Library_Section_Photo 
+	extends Plex_Server_Library_SectionAbstract
+{
+	// STUB! Need to implement this. This has been added to stop the library's
+	// getSection() method from crashing if it detects a photo section and
+	// attempts instantiation. This is on the back burner and scheduled to be
+	// put together later after the main library sections and client methods are
+	// complete.
+}
